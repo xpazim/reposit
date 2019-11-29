5e123f683c87fb9fd578666493cde3a08b7dd85c
xpazim <57419206+xpazim@users.noreply.github.com>
2019-11-28 21:53:47 -0300

    Update project23.py

--- project23.py
+++ project23.py
@@ -1,7 +1,6 @@
 #trampo de web
 
 import turtle
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -129,6 +128,4 @@ def Jogo():
             pen.write("Jogador 2 Venceu", align="center", font=("Arial", 15, "normal"))
             turtle.exitonclick()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
